2114ec9054d14136ed3716e17b89b4c3467a2151
AlexSilver <silver.alex.333@gmail.com>
2020-04-24 23:13:34 +0300

    Init commit

---
+++ src/pages/home/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './homepage.scss';
+
+import Directory from '../../components/directory'
+
+export const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
