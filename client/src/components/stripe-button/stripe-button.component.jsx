c65056d3544ba2c1e49eaa20c4ed2ef69f0d9b91
AlexSilver <silver.alex.333@gmail.com>
2020-06-14 16:37:20 +0300

    Adding axios and implementing payment process b2b

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 export const StripeButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_ASeMXxkL5wMDBKr1pd1rY56F00a18A3zNB';
 
   const onToken = (token) => {
-    console.log(token);
-    alert('Payment successful');
+    axios
+      .post('payment', {
+        amount: priceForStripe,
+        token: token,
+      })
+      .then((response) => {
+        alert('Payment successful');
+      })
+      .catch((error) => {
+        console.log('Payment error: ' + JSON.parse(error));
+        alert('There was an issue with your payment. Please sure you use the provided credit card.');
+      });
   };
 
   return (
